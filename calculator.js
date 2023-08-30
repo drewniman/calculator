c3fdc33982c2d756a962284ff6687dbc1bbccc76
Drew Niman <drewmniman@gmail.com>
2023-08-30 10:11:09 -0700

    Added calculator functions

---
+++ calculator.js
@@ -0,0 +1,27 @@
+// Standard notation
+
+// function add(a, b) {
+//     return a + b;
+// }
+
+// function subtract(a, b) {
+//     return a - b;
+// }
+
+// function multiply(a, b) {
+//     return a * b;
+// }
+
+// function divide(a, b) {
+//     return a / b;
+// }
+
+// Arrow functions
+
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => a / b;
