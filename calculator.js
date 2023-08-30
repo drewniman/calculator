891653cb42063c59f595522cce86a9b0dd8aff73
Drew Niman <drewmniman@gmail.com>
2023-08-30 10:17:56 -0700

    Added operate function

--- calculator.js
+++ calculator.js
@@ -1,27 +1,25 @@
-// Standard notation
-
-// function add(a, b) {
-//     return a + b;
-// }
-
-// function subtract(a, b) {
-//     return a - b;
-// }
-
-// function multiply(a, b) {
-//     return a * b;
-// }
-
-// function divide(a, b) {
-//     return a / b;
-// }
-
-// Arrow functions
-
+// Calculator functions
 const add = (a, b) => a + b;
-
 const subtract = (a, b) => a - b;
-
 const multiply = (a, b) => a * b;
-
 const divide = (a, b) => a / b;
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+        default:
+            return 'ERROR';
+    }
+}
+
+// Calculator variables
+let num1;
+let num2;
+let operator;
